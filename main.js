40577a7bc668debe42916ef7568648bd22bf4708
ShantanuMandpe-Bosch <fixed-term.Shantanu.Mandpe@bosch-sensortec.com>
2023-10-11 10:36:55 +0200

    main.js changes - runnable file

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 const fs = require('fs');
 const readLine = require('readline')
 var parquet = require('parquetjs');
-const { Int } = require('apache-arrow');
 
 const stream = fs.createReadStream('example.udf')
 const rl = readLine.createInterface({
@@ -21,29 +20,28 @@ let header = []
 
 
 let schema = {
-    sensorID : [],
-    sensorName : [],
-    eventSize : [],
-    parseFormat : [],
-    axisNames : [],
-    scalingFactor : []
+    sensorID: [],
+    sensorName: [],
+    eventSize: [],
+    parseFormat: [],
+    axisNames: [],
+    scalingFactor: []
 }
 
-var parquetSchema = new parquet.ParquetSchema({
-    ID: { type: 'UINT_8'},
-    /*Name: {typer: 'UTF-8'},
-    Size: {type: 'UTF-8'},
-    Format: {type: 'UTF-8'},
-    Axis: {type: 'UTF-8'},
-    Scaling: { type: 'FLOAT'},*/
-    Values: { type: 'UINT_8', repeated: true }
-})
+// var parquetSchema1 = new parquet.ParquetSchema({
+//     ID: { type: 'UINT_8' },
+//     Values: { type: 'INT_8', repeated: true }
+// })
+
+let schemaObject = {} //object for the schema
+
+// let parquetSchema = new parquet.ParquetSchema(testObj);
 
 rl.on('line', (line) => {
     const binaryData = Buffer.from(line)
 
-    if((/^(1.0|1.1)/).test(line)){
-        if(line == "1.0"){
+    if ((/^(1.0|1.1)/).test(line)) {
+        if (line == "1.0") {
             variable_schema = 0
             header.push(line)
             test = test + binaryData.byteLength
@@ -53,21 +51,23 @@ rl.on('line', (line) => {
             test = test + binaryData.byteLength
         }
     }
-    else if((/^\d:/).test(line) || (/^\d\d:/).test(line)) {
-        console.log("this is in udf format")
+    else if ((/^\d:/).test(line) || (/^\d\d:/).test(line)) {
         const split = line.split(":")
-        schema.sensorID.push(split[0]) 
-        schema.sensorName.push(split[1]) 
-        schema.eventSize.push(split[2]) 
-        schema.parseFormat.push(dataTypeEquivalent(split[3])) 
+        // add elements to the schema
+        schema.sensorID.push(split[0])
+        schema.sensorName.push(split[1])
+        schema.eventSize.push(split[2])
+        schema.parseFormat.push(dataTypeEquivalent(split[3]))
         schema.axisNames.push(split[4])
-        schema.scalingFactor.push(split[5]) 
+        schema.scalingFactor.push(split[5])
+
+        //add elements to the parquet-schema
         header.push(line)
         test = test + binaryData.byteLength
-        udf1 ++
+        udf1++
     } else {
-        console.log((test + header.length))
-    }          
+        //console.log((test + header.length))
+    }
 })
 
 rl.on('close', () => {
@@ -75,18 +75,14 @@ rl.on('close', () => {
     console.log("File reading complete")
 })
 
-function readFullFile(){
-    // Replace 'your-binary-file.bin' with the path to your binary file
+function readFullFile() {
     const filePath = 'example.udf';
-
-    // Use fs.readFile to read the binary file
     fs.readFile(filePath, (err, data) => {
         if (err) {
             console.error('Error reading file:', err);
             return;
         }
-        console.log(data)
-
+        
         const binaryData = Buffer.from(data)
         buffer = new ArrayBuffer(binaryData.byteLength)
         let uint8View = new Uint8Array(buffer)
@@ -95,26 +91,24 @@ function readFullFile(){
             uint8View[i] = binaryData[i]
             line.push(uint8View[i])
         }
-        //console.log(uint8View)
 
         let arr = []
         for (let i = 144; i < line.length; i++) {
             arr.push(line[i])
+            //arrData.push(data[i])
         }
-        console.log(arr)
-        console.log("ARR is :" + arr.indexOf(240))
 
+        //console.log("Arr " + arr)
+        
         let newArr = []
         for (let i = 17; i < arr.length; i++) {
             newArr.push(arr[i])
         }
-        console.log(newArr)
-        console.log("ARR is :" + newArr.indexOf(240))
 
         let counter = 0
 
-        while(newArr.length > 0){
-            if (counter == 0 || counter%(header.length+1) == 0){
+        while (newArr.length > 0) {
+            if (counter == 0 || counter % (header.length + 1) == 0) {
                 timeID.push(newArr.shift())
                 for (let j = 0; j < 8; j++) {
                     timeStamp.push(newArr.shift())
@@ -124,23 +118,44 @@ function readFullFile(){
                 sensorID.push(newArr.shift())
                 let id = schema.sensorID.indexOf(String(sensorID.at(-1)))
                 let valueSize = schema.eventSize[String(id)]
+                let parse = schema.parseFormat[String(id)]
+                let subArr = []
                 for (let j = 0; j < valueSize; j++) {
-                    sensorValue.push(newArr.shift())
+                    subArr.push(newArr.shift())
                 }
-                counter++
-            }    
-        }
+                ////////
+                if(parse == 'INT_8'){
+                    let uint8Array = new Uint8Array(subArr);
+                    let int8Array = new Int8Array(uint8Array.buffer);
+                    for(let i=0; i< int8Array.length;i++){
+                        sensorValue.push(int8Array[i])
+                    }
+                    //console.log(int8Array)
+                } else if (parse == 'INT_16'){
+                    let uint8Array = new Uint8Array(subArr);
+                    let int16Array = new Int16Array(uint8Array.buffer);
+                    for (let i = 0; i < int16Array.length; i++) {
+                        sensorValue.push(int16Array[i])
+                    }
+                    //console.log(int16Array)
+                } else {
+                    let uint8Array = new Uint8Array(subArr);
+                    let float32Array = new Float32Array(uint8Array.buffer);
+                    for (let i = 0; i < float32Array.length; i++) {
+                        sensorValue.push(float32Array[i])
+                    }
 
-        console.log(timeID.length)
-        console.log(timeStamp.length)
-        console.log(sensorID)
-        console.log(sensorValue)
+                }               
+                ///////
+                counter++
+            }
+        }
         convertToParquet(sensorID, sensorValue)
     });
-   
+
 }
 
-function convertToParquet(arr1,arr2) {
+function convertToParquet(arr1, arr2) {
     let sensorIDArr = []
     let sensorValueArr = []
     let length = (arr1.length)
@@ -148,16 +163,17 @@ function convertToParquet(arr1,arr2) {
         let x = arr1.shift()
         sensorIDArr.push(x)
         let id = schema.sensorID.indexOf(String(sensorIDArr.at(-1)))
-        let valueSize = schema.eventSize[String(id)]
+        let axis = schema.axisNames[String(id)].split(",")
         let subArr = []
-        for (let j = 0; j < parseInt(valueSize); j++) {
+        for (let j = 0; j < parseInt(axis.length); j++) { //this is hardcoded 
             subArr.push(arr2.shift())
         }
         sensorValueArr.push(subArr)
     }
-    console.log(sensorIDArr.length)
-    console.log(sensorValueArr)
+    //console.log(sensorIDArr.length)
+    
     try {
+        //console.log(sensorValueArr)
         toParquetFile(sensorIDArr, sensorValueArr)
     } catch (error) {
         console.error(error);
@@ -165,29 +181,42 @@ function convertToParquet(arr1,arr2) {
 }
 
 async function toParquetFile(dataID, dataValue) {
-    // create new ParquetWriter that writes to 'fruits.parquet`
-    var writer = await parquet.ParquetWriter.openFile(parquetSchema, 'main.parquet');
+
+    console.log(schemaObject)
+
+    // let parquetSchema = new parquet.ParquetSchema({schemaObject})
+    let parquetSchema = new parquet.ParquetSchema({
+        Name: { type: 'UTF8' },
+        A: { type: 'UTF8'},
+        B: { type: 'UTF8'},
+    })
+    
+    let writer = await parquet.ParquetWriter.openFile(parquetSchema, 'main.parquet')
     for (let i = 0; i < dataID.length; i++) {
         if (dataID[i] == 1) {
             await writer.appendRow({
-                ID: dataID[i],
-                Values: [dataValue[i][0], dataValue[i][1]]
+                Name: String(schema.sensorName[0]),
+                A: String(dataValue[i][0]),
+                B: String(dataValue[i][1]),
             })
         } else if (dataID[i] == 2) {
             await writer.appendRow({
-                ID: dataID[i],
-                Values: [dataValue[i][0], dataValue[i][1], dataValue[i][2], dataValue[i][3]]
+                Name: String(schema.sensorName[1]),
+                A: String(dataValue[i][0]),
+                B: String(dataValue[i][1]),
             })
-        } else if (dataID[i] == 3) {
+
+        } else if (dataID[i] == 3) {           
             await writer.appendRow({
-                ID: dataID[i],
-                Values: [dataValue[i][0], dataValue[i][1], dataValue[i][2], dataValue[i][3], dataValue[i][4], dataValue[i][5], dataValue[i][6], dataValue[i][7]]
-            })
+                Name: String(schema.sensorName[2]),
+                A: String(dataValue[i][0]),
+                B: String(dataValue[i][1]),
+            })        
         }
     }
     console.log(schema)
+
     await writer.close()
-    
 }
 
 function dataTypeEquivalent(data) {
@@ -195,37 +224,37 @@ function dataTypeEquivalent(data) {
     data.split(",").forEach((item) => {
         switch (item) {
             case ("s8"):
-                newData.push("Int8")
+                newData.push("INT_8")
                 break
             case "u8":
-                newData.push("Uint8")
+                newData.push("UINT_8")
                 break
             case "s16":
-                newData.push("Int16")
+                newData.push("INT_16")
                 break
             case "u16":
-                newData.push("Uint16")
+                newData.push("UINT_16")
                 break
             case "s32":
-                newData.push("Int32")
+                newData.push("INT_32")
                 break
             case "u32":
-                newData.push("Uint32")
+                newData.push("UINT_32")
                 break
             case "s64":
-                newData.push("Int64")
+                newData.push("INT_64")
                 break
             case "u64":
-                newData.push("Uint64")
+                newData.push("UINT_64")
                 break
             case "f":
-                newData.push("Float32")
+                newData.push("FLOAT")
                 break
             case "d":
-                newData.push("Float64")
+                newData.push("FLOAT")
                 break
             case "s":
-                newData.push("String")
+                newData.push("UTF8")
                 break
             default:
                 console.log("Error : This is not a accepted Parse Format")
@@ -235,4 +264,4 @@ function dataTypeEquivalent(data) {
     data = newData.join(",")
     return data
 }
-    
+
