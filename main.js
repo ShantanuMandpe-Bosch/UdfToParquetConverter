9c80734578eec45254ce9adfb9e7089f4f66d126
ShantanuMandpe-Bosch <fixed-term.Shantanu.Mandpe@bosch-sensortec.com>
2023-10-20 11:45:56 +0200

    bad changes

--- main.js
+++ main.js
@@ -1,88 +1,33 @@
-const form = document.querySelector('form');
-form.addEventListener('submit', handleSubmit);
+document.getElementById('modifyButton').addEventListener('click', function () {
+    const fileInput = document.getElementById('fileInput');
+    const newFilename = document.getElementById('newFilename').value;
 
-/** @param {Event} event */
-function handleSubmit(event) {
-
-    const form = event.currentTarget;
-    const url = new URL(form.action);
-    const formData = new FormData(form);
-    const searchParams = new URLSearchParams(formData);
-
-    const fetchOptions = {
-        method: form.method,
-    };
-    
-    if (form.method.toLowerCase() === 'post') {
-        if (form.enctype === 'multipart/form-data') {
-            fetchOptions.body = formData;
-        } else {
-          fetchOptions.body = searchParams;
-        }
-    } else {
-        url.search = searchParams;
+    if (!fileInput.files.length || !newFilename) {
+        alert('Please select a file and enter a new filename.');
+        return;
     }
 
-    try{
-        fetch(url, fetchOptions);
-    } catch(e){
-        console.error(e)
-    }
-    
-    event.preventDefault();
-}
+    const selectedFile = fileInput.files[0];
+    console.log('Selected File:', selectedFile.name); // Log the selected file's name to the console
 
-// // const input = document.querySelector('input[type="file"]');
-// // input.addEventListener('change', (e) => {
+    const formData = new FormData();
+    formData.append('file', fileInput.files[0]);
 
-// //   const fd = new FormData();
-
-// //   // add all selected files
-// //   fd.append(e.target.name, e.fil, e.name);  
-  
-// //   // create the request
-// //   const xhr = new XMLHttpRequest();
-// //   xhr.onload = () => {
-// //     if (xhr.status >= 200 && xhr.status < 300) {
-// //         // we done!
-// //     }
-// //   };
-  
-// //   // path to server would be where you'd normally post the form to
-// //   xhr.open('POST', '/modify', true);
-// //   xhr.send(fd);
-// // });
-// document.getElementById('modifyButton').addEventListener('click', function () {
-//     const fileInput = document.getElementById('fileInput');
-//     const newFilename = document.getElementById('newFilename').value;
-
-//     if (!fileInput.files.length || !newFilename) {
-//         alert('Please select a file and enter a new filename.');
-//         return;
-//     }
-
-//     const selectedFile = fileInput.files[0];
-//     console.log('Selected File:', selectedFile.name); // Log the selected file's name to the console
-
-//     const formData = new FormData();
-//     formData.append('file', fileInput.files[0]);
-
-//     fetch('/modify', {
-//         method: 'POST',
-//         body: formData,
-//         mode: "cors"
-//     })
-//         .then(response => response.blob())
-//         .then(blob => {
-//             const downloadLink = document.createElement('a');
-//             downloadLink.href = URL.createObjectURL(blob);
-//             downloadLink.download = newFilename;
-//             downloadLink.style.display = 'none';
-//             document.body.appendChild(downloadLink);
-//             downloadLink.click();
-//             document.body.removeChild(downloadLink);
-//         })
-//         .catch(error => {
-//             console.error('An error occurred:', error);
-//         });
-// });
+    fetch('/modify', {
+        method: 'POST',
+        body: formData,
+    })
+        .then(response => response.blob())
+        .then(blob => {
+            const downloadLink = document.createElement('a');
+            downloadLink.href = URL.createObjectURL(blob);
+            downloadLink.download = "path.parquet";
+            downloadLink.style.display = 'none';
+            document.body.appendChild(downloadLink);
+            downloadLink.click();
+            document.body.removeChild(downloadLink);
+        })
+        .catch(error => {
+            console.error('An error occurred:', error);
+        });
+});
--- server.js
+++ server.js
@@ -4,45 +4,313 @@ const fs = require('fs');
 const { v4: uuidv4 } = require('uuid');
 const path = require('path');
 const readLine = require('readline')
+var parquet = require('parquetjs');
+
+
+//declare variables
+let test = 0
+let start_uint8  = 240
+let downloadCheck = 0
+
+let schema = {
+    sensorID: [],
+    sensorName: [],
+    eventSize: [],
+    parseFormat: [],
+    axisNames: [],
+    scalingFactor: []
+}
+let schemaObject = {}
+let sensorTime = []
+let timeID = []
+let sensorID = []
+let sensorValue = []
+let header = []
+
+let outputPath = ""
 
 const app = express();
-const port = process.env.PORT || 5500;
+const port = process.env.PORT || 3000;
 
 app.use(fileUpload());
 
-//app.use(express.static(__dirname));
+app.use(express.static(__dirname));
 
-// function getSchemas(schema, schemaObject, header) {
-//     console.log(header)
-//     header.forEach((line) => {
-//         const split = line.split(":")
+function getSchemas() {
+    console.log(header)
+    header.forEach((line) => {
+        const split = line.split(":")
 
-//         // add elements to the main schema
-//         schema.sensorID.push(split[0])
-//         schema.sensorName.push(String(split[1]).trim())
-//         schema.eventSize.push(split[2])
-//         //schema.parseFormat.push(String(dataTypeEquivalent(split[3])).split(",").at(0))
-//         schema.parseFormat.push(((split[3])).split(",").at(0))
-//         schema.axisNames.push(String(split[4]).trim().toUpperCase())
-//         schema.scalingFactor.push(split[5])
-//     })
+        // add elements to the main schema
+        schema.sensorID.push(split[0])
+        schema.sensorName.push(String(split[1]).trim())
+        schema.eventSize.push(split[2])
+        schema.parseFormat.push(String(dataTypeEquivalent(split[3])).split(",").at(0))
+        //schema.parseFormat.push(((split[3])).split(",").at(0))
+        schema.axisNames.push(String(split[4]).trim().toUpperCase())
+        schema.scalingFactor.push(split[5])
+    })
 
-//     // add elements to the parquet object schema
-//     schema.sensorName.forEach((id) => {
-//         schemaObject[id] = {}
-//         schemaObject[id]["optional"] = true
-//         schemaObject[id]["fields"] = {}
-//         let axis = String(schema.axisNames.at(schema.sensorName.indexOf(id))).split(",")
-//         axis.forEach((axis) => {
-//             schemaObject[id]["fields"][axis.trim().toUpperCase()] = {}
-//             schemaObject[id]["fields"][axis.trim().toUpperCase()]["type"] = String(schema.parseFormat.at(schema.sensorName.indexOf(id)))
-//             schemaObject[id]["fields"][axis.trim().toUpperCase()]["optional"] = true
-//         })
-//     })
+    // add elements to the parquet object schema
+    schema.sensorName.forEach((id) => {
+        schemaObject[id] = {}
+        schemaObject[id]["optional"] = true
+        schemaObject[id]["fields"] = {}
+        let axis = String(schema.axisNames.at(schema.sensorName.indexOf(id))).split(",")
+        axis.forEach((axis) => {
+            schemaObject[id]["fields"][axis.trim().toUpperCase()] = {}
+            schemaObject[id]["fields"][axis.trim().toUpperCase()]["type"] = String(schema.parseFormat.at(schema.sensorName.indexOf(id)))
+            schemaObject[id]["fields"][axis.trim().toUpperCase()]["optional"] = true
+        })
+    })
+}
 
-//     console.log(schema)
-//     return schema
-// }
+function readFullFile(uploadPath,res){
+    fs.readFile(uploadPath, (err,data) => {
+        if(err){
+            console.error("Error Reading File", err)
+            return
+        }
+        const binaryData = Buffer.from(data)
+        buffer = new ArrayBuffer(binaryData.byteLength)
+        let uint8View = new Uint8Array(buffer)
+
+        let binaryDataByteLength = binaryData.byteLength
+        for (let i = 0; i < binaryDataByteLength; i++) {
+            uint8View[i] = binaryData[i]
+        }
+
+        // here we have some header part and rest is body
+        let subUint8View = uint8View.subarray(test) 
+
+        let identifierIndex = subUint8View.indexOf(start_uint8) //first entry of the value f0 - using as an identifier
+
+        let temp = subUint8View.subarray(identifierIndex)
+
+        let line = []
+        let tempLength = temp.length
+        for (let i = 0; i < tempLength; i++) {
+            line.push(temp[i])
+        }
+
+        // let searchArray = schema.sensorID
+        // searchArray.push(start_uint8)
+        // console.log(searchArray)
+
+        // let indexArray = []
+        // searchArray.forEach((i) => {
+        //     indexArray.push(line.indexOf(parseInt(240)))
+        // })
+        // console.log(indexArray)
+        // console.log(line)
+
+        // console.log()
+        formatCheck = line.includes(start_uint8)
+
+        let counter = 0
+        //assuming its timestamp id and start id are the same ids 
+        while (line.length > 0 && formatCheck==true) {
+            if(line[counter] == start_uint8){
+                timeID.push(line.splice(counter,1))
+                sensorTime.push(line.splice(counter,8))
+            } else {
+                sensorID.push(line.shift())
+                //console.log("SensorId : " + sensorID)
+
+                let id = schema.sensorID.indexOf(String(sensorID.at(-1)))
+                //console.log("id : " + id)
+                let valueSize = schema.eventSize[parseInt(id)]
+                //console.log("valueSize : " + parseInt(valueSize))
+                let parse = schema.parseFormat[String(id)]
+                //console.log("parse : " + parse)
+
+                let subArr = []
+                subArr = line.splice(counter, valueSize)
+
+                dataFormatting(parse,subArr)
+            }
+        }
+
+        while (line.length > 0 && formatCheck==false) {
+                sensorID.push(line.shift())
+                //console.log("SensorId : " + sensorID)
+
+                let id = schema.sensorID.indexOf(String(sensorID.at(-1)))
+                //console.log("id : " + id)
+                let valueSize = schema.eventSize[parseInt(id)]
+                //console.log("valueSize : " + parseInt(valueSize))
+                let parse = schema.parseFormat[String(id)]
+                //console.log("parse : " + parse)
+
+                let subArr = []
+                subArr = line.splice(counter, valueSize)
+
+                dataFormatting(parse,subArr)
+        }
+        
+        convertToParquet(sensorID, sensorValue,res)
+    })
+}
+
+function dataFormatting(parseFormat, arr){
+    if(parseFormat == 'INT_8'){
+        let uint8Array = new Uint8Array(arr);
+        let int8Array = new Int8Array(uint8Array.buffer);
+        sensorValue = [...sensorValue,...int8Array]
+    } else if (parseFormat == 'INT_16'){
+        let uint8Array = new Uint8Array(arr);
+        let int16Array = new Int16Array(uint8Array.buffer);
+        sensorValue = [...sensorValue,...int16Array]
+    } else if (parseFormat == 'UINT_8'){
+        let uint8Array = new Uint8Array(arr);
+        sensorValue = [...sensorValue,...uint8Array]
+    } else if (parseFormat == 'UINT_16'){
+        let uint8Array = new Uint8Array(arr);
+        let uint16Array = new Uint16Array(uint8Array.buffer);
+        sensorValue = [...sensorValue,...uint16Array]
+    } else if (parseFormat == 'INT_32'){
+        let uint8Array = new Uint8Array(arr);
+        let int32Array = new Int32Array(uint8Array.buffer);
+        sensorValue = [...sensorValue,...int32Array]
+    } else if (parseFormat == 'UINT_32'){
+        let uint8Array = new Uint8Array(arr);
+        let uint32Array = new Uint32Array(uint8Array.buffer);
+        sensorValue = [...sensorValue,...uint32Array]
+    } else if (parseFormat == 'INT_64'){
+        let uint8Array = new Uint8Array(arr);
+        let int64Array = new BigInt64Array(uint8Array.buffer);
+        sensorValue = [...sensorValue,...int64Array]
+    } else if (parseFormat == 'UINT_64'){
+        let uint8Array = new Uint8Array(arr);
+        let uint64Array = new BigUint64Array(uint8Array.buffer);
+        sensorValue = [...sensorValue,...uint64Array]
+    } else if (parseFormat == 'FLOAT'){
+        let uint8Array = new Uint8Array(arr);
+        let float32Array = new Float32Array(uint8Array.buffer);
+        sensorValue = [...sensorValue,...float32Array]
+    } else {
+        console.error("This is an invalid sensor Data Format")
+    }
+}
+
+async function convertToParquet(arr1, arr2,res){
+    let sensorIDArr = []
+    let sensorValueArr = []
+
+    for (let i = 0; i < arr1.length; i++) {
+        let x = arr1.shift()
+        //console.log("Value : " + x)
+        sensorIDArr.push(x)
+        let id = schema.sensorID.indexOf(String(sensorIDArr.at(-1)))
+        //console.log(id)
+        let axis = schema.axisNames[String(id)].split(",")  
+        let subArr = []
+        for (let j = 0; j < parseInt(axis.length); j++) { 
+            subArr.push(arr2.shift())
+        }
+        sensorValueArr.push(subArr)
+
+    }
+    //console.log(sensorIDArr)
+    //console.log(sensorValueArr)
+
+    let testArr = []
+    while(sensorValueArr.length >0){
+        let testArrSub = []
+        schema.sensorID.forEach( (i) => {
+            //console.log(i)
+            let id = sensorIDArr.indexOf(parseInt(i))
+            let length = String(schema.axisNames.at(schema.sensorID.indexOf(id))).split(",").length
+
+            if(id == -1){
+                let myArray = new Array(length).fill(0);
+                console.log(length)
+                testArrSub.push(myArray) 
+                //testArrSub.push([0,0]) //need to change - this is hard coded
+            } else {
+                sensorIDArr.splice(id,1)
+                testArrSub.push(sensorValueArr.splice(id,1)[0])  
+            }
+        })
+        testArr.push(testArrSub)
+    }
+    console.log(testArr)
+
+    let parquetSchema = new parquet.ParquetSchema(schemaObject)
+
+    let writer = await parquet.ParquetWriter.openFile(parquetSchema, "path.parquet")
+    console.log("created writer : " + path.parquet)
+    
+    let test1 = {}
+    let testArrLength = testArr.length
+    for (let i = 0; i < testArrLength; i = i + 1) {
+        schema.sensorName.forEach((id) => {
+            let axisList = String(schema.axisNames.at(schema.sensorName.indexOf(id))).split(",")
+            let new1 = {}
+            test1[id] = [] 
+            axisList.forEach((element) => {
+                let a = schema.sensorName.indexOf(id)
+                let b = axisList.indexOf(element)
+                new1[element] = testArr[i][parseInt(a)][parseInt(b)]
+            })
+            test1[id] = [new1] 
+        }) 
+        await writer.appendRow(test1)
+    } 
+
+    await writer.close()
+    console.log("writer closed")
+    downloadCheck = 1
+    console.log(res)
+}
+
+
+function dataTypeEquivalent(data) {
+    let newData = []
+    data.split(",").forEach((item) => {
+        switch (item) {
+            case ("s8"):
+                newData.push("INT_8")
+                break
+            case "u8":
+                newData.push("UINT_8")
+                break
+            case "s16":
+                newData.push("INT_16")
+                break
+            case "u16":
+                newData.push("UINT_16")
+                break
+            case "s32":
+                newData.push("INT_32")
+                break
+            case "u32":
+                newData.push("UINT_32")
+                break
+            case "s64":
+                newData.push("INT_64")
+                break
+            case "u64":
+                newData.push("UINT_64")
+                break
+            case "f":
+                newData.push("FLOAT")
+                break
+            case "d":
+                newData.push("FLOAT")
+                break
+            case "s":
+                newData.push("UTF8")
+                break
+            default:
+                console.log("Error : This is not a accepted Parse Format")
+                break
+        }
+    })
+    data = ""
+    data = newData.join(",")
+    return data
+}
 
 app.post('/modify', (req, res) => {
     if (!req.files || Object.keys(req.files).length === 0) {
@@ -52,28 +320,12 @@ app.post('/modify', (req, res) => {
     const uploadedFile = req.files.file;
     const newFilename = uuidv4() + path.extname(uploadedFile.name);
     const uploadPath = path.join(__dirname, 'uploads', newFilename);
-    const modifiedPath = path.join(__dirname, 'modified', newFilename);
+    let modifiedPath = path.join(__dirname,"path.parquet");
 
     uploadedFile.mv(uploadPath, (err) => {
         if (err) {
             return res.status(500).send(err);
         }
-
-        //declare variables
-        let test = 0
-        let newData1 = ""
-        let newData  = ""
-
-        let schema = {
-            sensorID: [],
-            sensorName: [],
-            eventSize: [],
-            parseFormat: [],
-            axisNames: [],
-            scalingFactor: []
-        }
-        let schemaObject = {}
-        let header = []
 
         const stream = fs.createReadStream(uploadPath)
         const rl = readLine.createInterface({
@@ -83,15 +335,14 @@ app.post('/modify', (req, res) => {
 
         console.log("Reading file lines")
 
-
         rl.on('line', (line) => {
             const binaryData = Buffer.from(line)
-            console.log(line)
 
             if ((/^(1.0|1.1)/).test(line)) {
                 if (line == "1.0") {
                     //variable_schema = 0
                     header.push(line)
+                    console.log(header)
                     test = test + binaryData.byteLength
                 } else if (line == "1.1") {
                     //variable_schema = 1
@@ -102,35 +353,29 @@ app.post('/modify', (req, res) => {
             else if ((/^\d:/).test(line) || (/^\d\d:/).test(line)) {
                 header.push(line)
                 test = test + binaryData.byteLength
-                //udf1++
             }
         })
 
         rl.on('close', () => {
-            //schema = getSchemas(schema,schemaObject,header)
+            getSchemas()
+            readFullFile(uploadPath,res)
             console.log(schema)
             console.log("File reading complete")
         })   
 
-        // Here, you can append data to the file using fs
-        const data = "new Uint8Array(Buffer.from(schema.axisNames));"
+        console.log("here")
 
-        fs.writeFile(uploadPath, data, (err) => {
+        // if(downloadCheck == 1){
+        //     console.log(modifiedPath)
+
+        //     });
+        // }
+
+        es.download(modifiedPath, (err) => {
             if (err) {
-                return res.status(500).send(err);
-            }
+        //        return res.status(500).send(err);
+        }
 
-            fs.rename(uploadPath, modifiedPath, (err) => {
-                if (err) {
-                    return res.status(500).send(err);
-                }
-                res.download(modifiedPath, uploadedFile.name, (err) => {
-                    if (err) {
-                        return res.status(500).send(err);
-                    }
-                });
-            });
-        });
     });
 });
 
